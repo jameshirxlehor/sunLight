9cf26f164a00eb02dad1b18e7ef31ac23d327a3e
r_souza <Rafaelrosod@outlook.com>
2024-02-19 22:06:28 -0300

    icone do app

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,13 @@ import 'package:sunlight/pages/home.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(
     MaterialApp(
-      home: Home(),
+      home: const Home(),
       theme: ThemeData(useMaterial3: false),
       debugShowCheckedModeBanner: false,
-
     ),
   );
 }
